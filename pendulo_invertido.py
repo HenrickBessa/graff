41fc15c4fb59bb4e155050b6e49626f17e851352
Henrick Bessa <henrickbessa2002@gmail.com>
2024-10-26 16:57:19 -0300

    simulacao

--- pendulo_invertido.py
+++ pendulo_invertido.py
@@ -3,30 +3,30 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 import matplotlib.pyplot as plt
 
-# Definindo variáveis fuzzy
+
 def criar_sistema_fuzzy():
     angulo = ctrl.Antecedent(np.arange(-10, 11, 1), 'angulo')
     velocidade_angular = ctrl.Antecedent(np.arange(-5, 6, 1), 'velocidade_angular')
     empurrao_carro = ctrl.Consequent(np.arange(-10, 11, 1), 'empurrao_carro')
 
-    # Funções de pertinência para ângulo
+   
     angulo['Inclinado Esquerda'] = fuzz.trimf(angulo.universe, [-10, -5, 0]) 
     angulo['Vertical'] = fuzz.trimf(angulo.universe, [-1, 0, 1])  
     angulo['Inclinado Direita'] = fuzz.trimf(angulo.universe, [0, 5, 10])   
 
-    # Funções de pertinência para velocidade angular
+   
     velocidade_angular['Deslocando-se Esquerda'] = fuzz.trimf(velocidade_angular.universe, [-5, -2, 0])  
     velocidade_angular['Parado'] = fuzz.trimf(velocidade_angular.universe, [-1, 0, 1])   
     velocidade_angular['Deslocando-se Direita'] = fuzz.trimf(velocidade_angular.universe, [0, 2, 5])    
 
-    # Funções de pertinência para empurrão no carro
+   
     empurrao_carro['Forte Esquerda'] = fuzz.trimf(empurrao_carro.universe, [-10, -7, -3])
     empurrao_carro['Leve Esquerda'] = fuzz.trimf(empurrao_carro.universe, [-5, -3, 0])
     empurrao_carro['Neutro'] = fuzz.trimf(empurrao_carro.universe, [-1, 0, 1])
     empurrao_carro['Leve Direita'] = fuzz.trimf(empurrao_carro.universe, [0, 3, 5])
     empurrao_carro['Forte Direita'] = fuzz.trimf(empurrao_carro.universe, [3, 7, 10])
 
-    # Regras fuzzy
+   
     rule1 = ctrl.Rule(angulo['Inclinado Esquerda'] & velocidade_angular['Deslocando-se Esquerda'], empurrao_carro['Forte Esquerda'])
     rule2 = ctrl.Rule(angulo['Inclinado Esquerda'] & velocidade_angular['Parado'], empurrao_carro['Leve Esquerda'])
     rule3 = ctrl.Rule(angulo['Inclinado Esquerda'] & velocidade_angular['Deslocando-se Direita'], empurrao_carro['Neutro'])
@@ -40,7 +40,6 @@ def criar_sistema_fuzzy():
     pendulo_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, rule9])
     return pendulo_ctrl
 
-# Função para plotar as funções de pertinência
 def plot_memberships(angulo, velocidade_angular, empurrao_carro):
     fig, (ax0, ax1, ax2) = plt.subplots(nrows=3, figsize=(8, 10))
 
@@ -68,28 +67,28 @@ def plot_memberships(angulo, velocidade_angular, empurrao_carro):
     plt.savefig('membership_functions_combined.png') 
     plt.close()
 
-# Função para simular o pêndulo
+
 def simular_pendulo(tempo_total, dt):
     tempos = np.arange(0, tempo_total, dt)
     angulos = []
     velocidades = []
 
-    angulo_atual = 0  # Ângulo inicial
-    velocidade_atual = 0  # Velocidade angular inicial
+    angulo_atual = 0  
+    velocidade_atual = 0 
 
     pendulo_ctrl = criar_sistema_fuzzy()
     pendulo_simulacao = ctrl.ControlSystemSimulation(pendulo_ctrl)
 
     for t in tempos:
-        # Atualizar as entradas do sistema fuzzy
+   
         pendulo_simulacao.input['angulo'] = angulo_atual
         pendulo_simulacao.input['velocidade_angular'] = velocidade_atual
 
-        # Computar a saída do sistema fuzzy
+    
         pendulo_simulacao.compute()
         empurro = pendulo_simulacao.output['empurrao_carro']
 
-        # Atualizar os estados do pêndulo
+      
         angulo_atual += velocidade_atual * dt
         velocidade_atual += empurro * dt  
 
@@ -98,10 +97,10 @@ def simular_pendulo(tempo_total, dt):
 
     return tempos, angulos, velocidades
 
-# Simular o pêndulo
+
 tempos, angulos, velocidades = simular_pendulo(10, 0.1)
 
-# Plotar resultados
+
 plt.figure(figsize=(10, 5))
 plt.subplot(2, 1, 1)
 plt.plot(tempos, angulos, label='Ângulo do Pêndulo')
@@ -119,7 +118,7 @@ plt.tight_layout()
 plt.savefig('simulacao_pendulo.png')
 plt.show()
 
-# Plotar funções de pertinência
+
 plot_memberships(ctrl.Antecedent(np.arange(-10, 11, 1), 'angulo'),
                  ctrl.Antecedent(np.arange(-5, 6, 1), 'velocidade_angular'),
                  ctrl.Consequent(np.arange(-10, 11, 1), 'empurrao_carro'))
--- pendulo_invertido_simulacao.py
+++ pendulo_invertido_simulacao.py
@@ -5,14 +5,12 @@ import matplotlib.pyplot as plt
 
 class PenduloInvertido:
     def __init__(self):
-        # Variáveis de entrada
+
         self.angulo = ctrl.Antecedent(np.arange(-180, 181, 1), 'angulo')
         self.velocidade_angular = ctrl.Antecedent(np.arange(-50, 51, 1), 'velocidade_angular')
 
-        # Variável de saída
         self.empurro_carro = ctrl.Consequent(np.arange(-10, 11, 1), 'empurrao_carro')
 
-        # Definindo funções de pertinência
         self.angulo['negativo'] = fuzz.trimf(self.angulo.universe, [-180, -90, 0])
         self.angulo['zero'] = fuzz.trimf(self.angulo.universe, [-90, 0, 90])
         self.angulo['positivo'] = fuzz.trimf(self.angulo.universe, [0, 90, 180])
@@ -25,7 +23,6 @@ class PenduloInvertido:
         self.empurro_carro['zero'] = fuzz.trimf(self.empurro_carro.universe, [-5, 0, 5])
         self.empurro_carro['direita'] = fuzz.trimf(self.empurro_carro.universe, [0, 5, 10])
 
-        # Regras
         self.regras = [
             ctrl.Rule(self.angulo['negativo'] & self.velocidade_angular['negativa'], self.empurro_carro['direita']),
             ctrl.Rule(self.angulo['negativo'] & self.velocidade_angular['zero'], self.empurro_carro['direita']),
@@ -38,7 +35,6 @@ class PenduloInvertido:
             ctrl.Rule(self.angulo['positivo'] & self.velocidade_angular['positiva'], self.empurro_carro['esquerda']),
         ]
 
-        # Sistema de controle
         self.sistema_controle = ctrl.ControlSystem(self.regras)
         self.simulador = ctrl.ControlSystemSimulation(self.sistema_controle)
 
@@ -47,18 +43,17 @@ class PenduloInvertido:
         self.simulador.input['velocidade_angular'] = velocidade_angular
         self.simulador.compute()
         
-        # Garantir que a saída seja sempre acessível
         if 'empurrao_carro' in self.simulador.output:
             return self.simulador.output['empurrao_carro']
         else:
-            return 0  # Ou outro valor padrão
+            return 0 
 
 def simular_pendulo(tempo_total, intervalo):
     pendulo_simulacao = PenduloInvertido()
 
-    # Inicializa as variáveis de estado
-    angulo = 10  # ângulo inicial em graus
-    velocidade_angular = 0  # velocidade angular inicial em graus por segundo
+  
+    angulo = 10
+    velocidade_angular = 0 
     tempos = []
     angulos = []
     velocidades = []
@@ -66,12 +61,12 @@ def simular_pendulo(tempo_total, intervalo):
     for t in np.arange(0, tempo_total, intervalo):
         empurro = pendulo_simulacao.controlar_pendulo(angulo, velocidade_angular)
 
-        # Atualiza a física do pêndulo (simulação simples)
-        aceleração = empurro  # Aceleração proporcional à força aplicada
+  
+        aceleração = empurro
         velocidade_angular += aceleração * intervalo
         angulo += velocidade_angular * intervalo
 
-        # Salva os estados para visualização
+      
         tempos.append(t)
         angulos.append(angulo)
         velocidades.append(velocidade_angular)
@@ -80,10 +75,10 @@ def simular_pendulo(tempo_total, intervalo):
 
     return tempos, angulos, velocidades
 
-# Execução da simulação
+
 tempos, angulos, velocidades = simular_pendulo(10, 0.1)
 
-# Gráfico dos resultados
+
 plt.figure(figsize=(12, 6))
 plt.subplot(2, 1, 1)
 plt.plot(tempos, angulos, label='Ângulo do Pêndulo', color='b')
